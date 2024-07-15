3e61c1ee2bff205fed5df2aa74cf42266f3c8786
Charvit Zalavadiya <charvitzalavadiya777@gmail.com>
2024-07-15 17:52:29 +0530

    Final change without using useMemo()

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 // import cors from 'cors';
-import cors from 'micro-cors';
 
 const nextConfig = {
     reactStrictMode: true,
-    cors: {
-      origin: '*',
-      methods: ['GET', 'POST', 'PUT', 'DELETE'],
-      headers: ['Content-Type', 'Authorization'],
-    },
 };
 
 export default nextConfig;
